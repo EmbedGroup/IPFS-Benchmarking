304ff9e3070ba8ff5eb8393ef5aaafd160c4ee0d
EmbedGroup <embedgroup2@gmail.com>
2022-01-27 16:50:35 +0800

    go version

---
+++ tool/gov.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
+useage(){
+  cat <<"EOF"
+USAGE:
+    gov.sh VERSION
+EOF
+}
+
+if [ $# -lt 1 ];then
+    useage
+    exit
+fi
+
+GOROOT="/usr/local/go"
+sudo rm -rf "${GOROOT}"
+VERSION=$1
+GOPACK="${GOROOT}${VERSION}"
+if [ ! -d "${GOPACK}" ];then
+  sudo wget "https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz" -cP "${HOME}/Downloads/"
+  TEMPDIR=$(mktemp -d)
+  tar -zxf "${HOME}/Downloads/go${VERSION}.linux-amd64.tar.gz" -C "${TEMPDIR}"
+  mkdir -p "${TEMPDIR}"
+  sudo mv "${TEMPDIR}/go" "${GOPACK}"
+  sudo rm -r "${TEMPDIR}"
+fi
+sudo ln -s "${GOPACK}" "${GOROOT}"
+GOVERSIONNOW="$(go version | awk '{print $3}' | sed 's/go//g')"
+echo "go ${GOVERSIONNOW}"
